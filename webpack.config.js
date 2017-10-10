811b03b722d14ddb0085570fc7982ecc7b70bb00
Shon T. Urbas <shon.urbas@gmail.com>
2017-10-09 21:18:27 -0400

    production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,49 @@
+const webpack = require('webpack');
+
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
-  entry:  './src/components/LazyLog/index.js',
-  output: {
+    entry: './src/components/LazyLog/index.js',
+    output: {
         path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      libraryTarget: 'commonjs-module',
-      library: 'react-lazylog'
+        filename: 'bundle.js',
+        libraryTarget: 'commonjs-module',
+        library: 'react-lazylog'
     },
-  module: {
-    loaders: [
-      {
-        test: /\.js/,
-        loader: 'babel-loader',
-        include: __dirname + '/src',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true, // default is false
-              sourceMap: true,
-              importLoaders: 1,
-              localIdentName: "[name]--[local]--[hash:base64:8]"
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader',
+                include: __dirname + '/src',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true, // default is false
+                            sourceMap: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]--[local]--[hash:base64:8]"
+                        }
+                    },
+                    "postcss-loader"
+                ]
             }
-          },
-          "postcss-loader"
-        ]
-      }
-    ],
-  },
-  plugins: [
-      new ExtractTextPlugin("styles.css"),
-  ]
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
